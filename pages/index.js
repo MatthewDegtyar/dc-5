db74a403d39a53d0fcc3590e777c76066300d997
Matthew <mattdegtyar@gmail.com>
2024-01-26 14:58:12 -0500

    26 jan 24 - more little fixes, hero

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,8 @@
 import RootLayout from '../styles/RootLayout'
 import React, { useEffect, useRef } from 'react'
-{/** 
-const EmbedVideo = (props) => {
-    const videoRef = useRef(null);
-  
-    useEffect(() => {
-      const video = videoRef.current;
-  
-      const playVideoOnLoad = () => {
-        if (video) {
-          video.play().catch((error) => {
-            // Autoplay was prevented, handle the error if needed
-            console.error('Autoplay prevented:', error);
-          });
-        }
-      };
-  
-      playVideoOnLoad();
-  
-      return () => {
-        // Clean up any event listeners or resources if needed
-      };
-    }, []);
-  
-    return (
-      <video
-        ref={videoRef}
-        className={props.className}
-        playsInline
-        loop
-        muted
-      >
-  
-        <source src={props.src} type="video/mp4" />
-        <p className='text-white'></p>
-        Your browser does not support the video tag.
-      </video> 
-    );
-  };
-*/}
+
 const Home = () => {
+
   return (
     <RootLayout meta_description='Professional website design' canon_url='/' imageUrl='/img/DClogo1024.webp' pageTitle='Home'>
         <div className='flex flex-col lg:flex-row bg-black'>
@@ -49,7 +12,7 @@ const Home = () => {
                 <iframe
                      width="100%"
                      height="100%"
-                     src="/svg/hero7.svg"
+                     src="/svg/hero9.svg"
                      frameBorder="0"
                      allowFullScreen
                   ></iframe>
@@ -57,8 +20,8 @@ const Home = () => {
             </div>
             <div className='text-black lg:w-[50vw] lg:h-[50vh] bg- flex flex-col lg:items-center items-start lg:justify-center place-items-center'>
                 <div className='flex text-start items-start lg:items-center flex-col p-4 md:p-8 rounded-[1px] gap-2'>
-                    <h1 className='text-title text-white  self-start text-nowrap'>We Create Value</h1>
-                    <h2 className='text-white mt-4 lg:mt-8 lg:w-96 '>We’re a next generation digital solutions provider eliminating point points and empowring businesses.</h2>
+                    <h1 className='text-title fade-in  text-white self-start text-nowrap'>We Create Value</h1>
+                    <h2 className='text-white fade-in  mt-4 lg:mt-8 lg:w-96 '>We’re a next generation digital solutions provider eliminating point points and empowring businesses.</h2>
                 </div>
             </div>
         </div>
