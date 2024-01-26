8c0dc34a878dbb78332ea6f2bdb11ea313363037
Matthew <mattdegtyar@gmail.com>
2024-01-26 02:54:58 -0500

    26 jan 24 - third

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ const EmbedVideo = (props) => {
         muted
       >
   
-        <source src={props.src} type="video/webm" />
+        <source src={props.src} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     );
@@ -45,11 +45,11 @@ const Home = () => {
 
             <div className='lg:w-[50vw] h-[50vh] md:h-auto lg:h-[50vh] overflow-hidden rounded-[1px] flex flex-col'>
                 <div className='lg:w-[50vw]  lg:h-[40vh]'>
-                <EmbedVideo src='/video/hero.webm' className="lg:h-[50vh] md:h-auto w-full h-[50vh] z-0 object-cover w-fit" />
+                <EmbedVideo src='/video/hero.mp4' className="lg:h-[50vh] md:h-auto w-full h-[50vh] z-0 object-cover w-fit" />
                 </div>
             </div>
             <div className='text-black lg:w-[50vw] lg:h-[50vh] bg- flex flex-col lg:items-center items-start lg:justify-center place-items-center'>
-                <div className='flex text-start w-96 items-start lg:w-auto lg:items-center flex-col p-4 md:p-8 rounded-[1px] gap-2'>
+                <div className='flex text-start items-start lg:items-center flex-col p-4 md:p-8 rounded-[1px] gap-2'>
                     <h1 className='text-title text-white uppercase self-start text-nowrap'>We Create Value</h1>
                     <h2 className='text-white mt-4 lg:mt-8 w-96 '>We’re a creative digital agency creating meaningful digital presence and empowering businesses.</h2>
                 </div>
