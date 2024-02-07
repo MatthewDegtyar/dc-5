65fcc290cdc809558a19e069ecd0ef754c3ed23b
matthewdegtyar <mattdegtyar@gmail.com>
2024-02-07 12:02:49 -0500

    7 feb 24 - analytics update

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import {NextUIProvider} from "@nextui-org/react";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return     <NextUIProvider><Component {...pageProps} /></NextUIProvider>
+  return (     
+    <NextUIProvider>
+      <Component {...pageProps} />
+      <Analytics/>
+    </NextUIProvider>
+  )
 }
 
 export default MyApp
